9d56d0186e076f38958f2e5356e40f2b7f024e2b
Peter <peter@peter-debian.local>
2018-05-17 15:11:44 +0200

    modify function

--- create/index.js
+++ create/index.js
@@ -1,15 +1,20 @@
 'use strict';
-const prefix = "mysite.com";
-const base_page = `<html>
-<body>
-    <h3>URL has been shortened:
-        <a href="https://${prefix}/fake">${prefix}/fake</a>
-    </h3>
-</body>
-</html>'`
+
+const querystring = require('querystring');
 
 module.exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
+    const submitted = querystring.parse(event.body).link;
+    const prefix = event.headers.Referer || 'http://mysite.com';
+    const base_page = `<html>
+<body>
+    <h3>URL ${submitted} has been shortened:
+        <a href="https://${prefix}/fake">${prefix}/fake</a>
+    </h3>
+</body>
+</html>`;
+
+    console.log('URL submitted: ' + submitted);
     callback(
         null,
         {
