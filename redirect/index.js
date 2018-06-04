61407b5c49a98afb707e0800822c2c1ef0ecc470
Peter <peter@peter-debian.local>
2018-06-04 13:18:16 +0200

    update

--- redirect/index.js
+++ redirect/index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const AWS = require('aws-sdk')
 
@@ -6,45 +6,44 @@ const tableName = `${process.env.SLS_STAGE}-shortened-urls`
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 module.exports.handler = (event, context, callback) => {
-    console.log(JSON.stringify(event))
+  console.log(JSON.stringify(event))
 
-    const slug = event.pathParameters.slug
+  const slug = event.pathParameters.slug
 
-    docClient.get(
-    {
-        TableName: tableName,
-        Key: {slug:slug}
-    }, (err,data) => 
-    {
-        console.log(data)
-        if(err) {
-            console.log(err)
-            return callback(err)
+  docClient.get({
+    TableName: tableName,
+    Key: {
+      slug: slug
+    }
+  }, (err, data) => {
+    console.log(data)
+    if (err) {
+      console.log(err)
+      return callback(err)
+    }
+    if (data.Item) {
+      callback(
+        null,
+        {
+          statusCode: 302,
+          body: data.Item.long_url,
+          headers: {
+            'Location': data.Item.long_url,
+            'Content-Type': 'text/plain'
+          }
         }
-        if(data.Item) {
-            callback(
-                null,
-                {
-                    statusCode: 302,
-                    body: data.Item.long_url,
-                    headers:{
-                        'Location': data.Item.long_url,
-                        'Content-Type': 'text/plain'
-                    }
-                }
-            )
-        }else{
-            callback(
-                null,
-                {
-                    statusCode:404,
-                    body: "This shorened link doesn't exist, check that you entered the it right.",
-                    headers:{
-                        'Content-Type': 'text/plain'
-                    }
-                }
-            )
+      )
+    } else {
+      callback(
+        null,
+        {
+          statusCode: 404,
+          body: "This shortened link doesn't exist, check that you entered it right.",
+          headers: {
+            'Content-Type': 'text/plain'
+          }
         }
-    })
-    
+      )
+    }
+  })
 }
